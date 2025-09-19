2e5da92f6846495a8f389a89ea65cfaee7c32183
“[Godwin <179575272+GodwinAkhidue@users.noreply.github.com>
2025-09-19 22:00:25 +0100

    .

--- app/create_tables/users/route.js
+++ app/create_tables/users/route.js
@@ -9,7 +9,7 @@ export default async function Users() {
   }
 
   const query = `CREATE TABLE IF NOT EXISTS users (
-        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
         email VARCHAR UNIQUE,
         password VARCHAR,
         role VARCHAR,
--- app/create_tables/users/uuid-ossp.js
+++ app/create_tables/users/uuid-ossp.js
@@ -1,7 +1,7 @@
 import { Query_Psql_DB } from "../../../config/psql_config.js";
 
 export default async function uuid_ossp() {
-  const query = `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`;
+  const query = `CREATE EXTENSION IF NOT EXISTS pgcrypto;`;
   //Create uuid extension on postgresql database
   //This extension helps to auto-generate random uuid in postgresql
 
