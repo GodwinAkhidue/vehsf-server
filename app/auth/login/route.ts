6ccafafb7c385f1347e4698791d2f20f26281d91
“[Godwin <179575272+GodwinAkhidue@users.noreply.github.com>
2025-09-19 14:02:44 +0100

    .

---
+++ app/auth/login/route.ts
@@ -0,0 +1,57 @@
+import express from "express";
+import { Query_Psql_DB } from "../../../config/psql_config";
+import argon2 from "argon2";
+import generateToken from "../../../config/jwt";
+
+const login = express();
+login.use(express.json());
+
+login.post(`/api/auth/login`, async (req, res) => {
+  const { email, password } = req.body;
+  const query = `SELECT id, password
+         FROM users
+         WHERE email = $1;`;
+  const values = [email];
+
+  const { error, result } = await Query_Psql_DB(query, values);
+
+  if (error) {
+    return res
+      .status(200)
+      .json({ success: false, message: "Something went wrong, try again" });
+  }
+
+  if (result.rows.length <= 0) {
+    return res
+      .status(200)
+      .json({ success: false, message: "Invalid Credentials" });
+  }
+
+  const hashedPassword = result.rows[0].password;
+
+  try {
+    const match = await argon2.verify(hashedPassword, password);
+    if (match) {
+      const id = result.rows[0].id;
+      const token = generateToken(id);
+
+      res.cookie("authToken", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 604800000, //7 days in milliseconds
+      });
+
+      return res.status(200).json({ success: true });
+    }
+    return res
+      .status(200)
+      .json({ success: false, message: "Invalid Credentials" });
+  } catch {
+    return res
+      .status(200)
+      .json({ success: false, message: "Something went wrong, try again" });
+  }
+});
+
+export default login;
--- app/auth/route.ts
+++ app/auth/route.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import signup from "./signup/route";
+import login from "./login/route";
 
 const auth = express();
 auth.use(express.json());
 auth.use("", signup);
+auth.use("", login);
 
 export default auth;
