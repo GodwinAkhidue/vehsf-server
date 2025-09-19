f5672778148a9c91c358e85938aa931db519e5c3
“[Godwin <179575272+GodwinAkhidue@users.noreply.github.com>
2025-09-19 14:18:34 +0100

    .

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import root from "./app";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://vehsf.com", credentials: true }));
 app.use(express.json());
 
 app.use("", root);
